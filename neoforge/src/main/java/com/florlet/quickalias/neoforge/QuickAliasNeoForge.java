4b025fa99ca9effb37f23d750066c207a5200f6d
Florlet <weihx15@gmail.com>
2025-12-08 23:37:00 +0800

    🎉 init: Port to Minecraft 1.21

--- neoforge/src/main/java/com/florlet/quickalias/neoforge/QuickAliasNeoForge.java
+++ neoforge/src/main/java/com/florlet/quickalias/neoforge/QuickAliasNeoForge.java
@@ -2,11 +2,14 @@ package com.florlet.quickalias.neoforge;
 
 import com.florlet.quickalias.QuickAliasClient;
 import com.florlet.quickalias.gui.SettingsScreen;
+import net.minecraft.client.gui.screens.Screen;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import org.jetbrains.annotations.NotNull;
 
 @Mod (QuickAliasClient.MOD_ID)
 public class QuickAliasNeoForge {
@@ -14,8 +17,15 @@ public class QuickAliasNeoForge {
         if (FMLEnvironment.dist == Dist.CLIENT) {
             QuickAliasClient.init();
 
-            ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class,
-                                                           () -> (mc, parent) -> new SettingsScreen(parent));
+            ModLoadingContext.get()
+                    .registerExtensionPoint(IConfigScreenFactory.class, QuickAliasConfigScreenFactory::new);
+        }
+    }
+
+    private static class QuickAliasConfigScreenFactory implements IConfigScreenFactory {
+        @Override
+        public @NotNull Screen createScreen(@NotNull ModContainer mod, @NotNull Screen parent) {
+            return new SettingsScreen(parent);
         }
     }
 }
